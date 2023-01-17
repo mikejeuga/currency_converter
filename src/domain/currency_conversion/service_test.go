dbe8c79ee1fe30ad7e4b9d9b922017d6da28c67b
Michael Jeuga <michael.jeuga@saltpay.co>
2023-01-17 15:51:29 +0000

    No more generated mock for service test

--- src/domain/currency_conversion/service_test.go
+++ src/domain/currency_conversion/service_test.go
@@ -6,36 +6,26 @@ import (
 	"github.com/alecthomas/assert"
 	"github.com/mikejeuga/currency_converter/models"
 	"github.com/mikejeuga/currency_converter/src/domain/currency_conversion"
-	"github.com/mikejeuga/currency_converter/src/domain/currency_conversion/mocks"
 	"testing"
 )
 
 func TestGateway(t *testing.T) {
-	deps := CreateDeps()
-	gateway := currency_conversion.NewService(deps.ConversionerMock)
+	client := &ClientStub{}
+	gateway := currency_conversion.NewService(client)
 
-	expectedFXRate := 0.92
-	givenGetRateWasCalled(deps, expectedFXRate)
+	expectedFXRate, err := client.GetFXRate(models.GBP, models.USD)
+	assert.NoError(t, err)
 
 	rate, err := gateway.GetRate(models.GBP, models.USD)
 	assert.NoError(t, err)
-	assert.Equal(t, expectedFXRate, rate.Spot)
+	assert.Equal(t, expectedFXRate.Spot, rate.Spot)
+
 }
 
-func givenGetRateWasCalled(deps Deps, fxRate float64) {
-	deps.ConversionerMock.GetFXRateFunc = func(base string, foreign string) (models.Rate, error) {
-		return models.Rate{
-			Spot: fxRate,
-		}, nil
-	}
-}
+type ClientStub struct{}
 
-type Deps struct {
-	ConversionerMock *mocks.ConversionerMock
-}
-
-func CreateDeps() Deps {
-	return Deps{
-		ConversionerMock: &mocks.ConversionerMock{},
-	}
+func (c ClientStub) GetFXRate(base, foreign string) (models.Rate, error) {
+	return models.Rate{
+		Spot: 0.92,
+	}, nil
 }
