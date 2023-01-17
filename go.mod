d2797d75fb44d68f27162f662b11240cf60336b9
Michael Jeuga <michael.jeuga@saltpay.co>
2023-01-17 16:05:17 +0000

    cleaner for now

--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/mikejeuga/currency_converter
 go 1.19
 
 require (
-	github.com/alecthomas/assert v1.0.0 // indirect
+	github.com/alecthomas/assert v1.0.0
+	github.com/alecthomas/assert/v2 v2.2.1
+	github.com/gorilla/mux v1.8.0
+	github.com/kelseyhightower/envconfig v1.4.0
+)
+
+require (
 	github.com/alecthomas/colour v0.1.0 // indirect
-	github.com/alecthomas/repr v0.0.0-20210801044451-80ca428c5142 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/alecthomas/repr v0.2.0 // indirect
+	github.com/hexops/gotextdiff v1.0.3 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- src/domain/currency_conversion/service_test.go
+++ src/domain/currency_conversion/service_test.go
@@ -3,29 +3,33 @@
 package currency_conversion_test
 
 import (
-	"github.com/alecthomas/assert"
+	"github.com/alecthomas/assert/v2"
 	"github.com/mikejeuga/currency_converter/models"
 	"github.com/mikejeuga/currency_converter/src/domain/currency_conversion"
 	"testing"
 )
 
 func TestGateway(t *testing.T) {
-	client := &ClientStub{}
+	client := NewClientStub(0.92)
 	gateway := currency_conversion.NewService(client)
-
-	expectedFXRate, err := client.GetFXRate(models.GBP, models.USD)
-	assert.NoError(t, err)
 
 	rate, err := gateway.GetRate(models.GBP, models.USD)
 	assert.NoError(t, err)
-	assert.Equal(t, expectedFXRate.Spot, rate.Spot)
+	assert.Equal(t, client.Spot.Spot, rate.Spot)
 
 }
 
-type ClientStub struct{}
+type ClientStub struct {
+	Spot models.Rate
+}
+
+func NewClientStub(spot float64) *ClientStub {
+
+	return &ClientStub{Spot: models.Rate{
+		Spot: spot,
+	}}
+}
 
 func (c ClientStub) GetFXRate(base, foreign string) (models.Rate, error) {
-	return models.Rate{
-		Spot: 0.92,
-	}, nil
+	return c.Spot, nil
 }
